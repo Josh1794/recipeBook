1abbb88d5b35318737b08e078bc1c6cd600f6187
Joshua Grossman <Josh1794@gmail.com>
2020-01-25 22:20:35 -0500

    added routes and redux work

---
+++ client/store/book.js
@@ -0,0 +1,54 @@
+import axios from "axios";
+
+/**
+ * ACTION TYPES
+ */
+const GOT_ALL_BOOKS = "GOT_ALL_BOOKS";
+const GOT_SINGLE_BOOK = "GOT_SINGLE_BOOK";
+
+/**
+ * ACTION CREATORS
+ */
+const gotAllBooks = books => ({ type: GOT_ALL_BOOKS, books });
+const gotSingleBook = products => ({ type: GOT_SINGLE_PRODUCT, products });
+
+/**
+ * THUNK CREATORS
+ */
+export const getAllBooks = () => async dispatch => {
+  try {
+    const { data } = await axios.get("/api/books");
+    dispatch(gotAllBooks(data));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const getSingleBook = id => async dispatch => {
+  try {
+    const { data } = await axios.get(`/api/books/${id}`);
+    dispatch(gotSingleBook(data));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+/**
+ * INITIAL STATE
+ */
+const initialState = { books: [], singleBook: {} };
+
+/**
+ * REDUCER
+ */
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case GOT_ALL_BOOKS:
+      return { ...state, books: [...action.books] };
+    case GOT_SINGLE_BOOK:
+      return { ...state, singleBook: { ...action.books } };
+    default:
+      return state;
+  }
+};
--- server/api/books.js
+++ server/api/books.js
@@ -1,17 +1,23 @@
-const router = require('express').Router()
-const {Book} = require('../db/models')
-module.exports = router
+const router = require("express").Router();
+const { Book } = require("../db/models");
+module.exports = router;
 
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
-    const books = await Book.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['name', 'userId']
-    })
-    res.json(books)
+    const books = await Book.findAll();
+    if (books) res.json(books);
+    else res.sendStatus(500);
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const singleBook = await Book.findByPk(req.params.id);
+    if (singleBook) res.send(singleBook);
+    else res.status(404).send("404");
+  } catch (err) {
+    next(err);
+  }
+});
