57ab5c4bccb3207d49218c0498d2650862dab1c0
Joshua Grossman <Josh1794@gmail.com>
2020-02-19 16:01:08 -0500

    webpack changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,31 @@
-const isDev = process.env.NODE_ENV === 'development'
+const isDev = process.env.NODE_ENV === "development";
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: isDev ? 'development' : 'production',
+  mode: isDev ? "development" : "production",
   entry: [
-    '@babel/polyfill', // enables async-await
-    './client/index.js'
+    "@babel/polyfill", // enables async-await
+    "./client/index.js"
+  ],
+  plugins: [
+    new CleanWebpackPlugin(["dist"]),
+    new HtmlWebpackPlugin({
+      title: "Output Management",
+      title: "Caching"
+    })
   ],
   output: {
     path: __dirname,
-    filename: './public/bundle.js'
+    filename: "./public/bundle.js",
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist")
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"]
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   watchOptions: {
     ignored: /node_modules/
   },
@@ -22,8 +34,8 @@ module.exports = {
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
+        loader: "babel-loader"
       }
     ]
   }
-}
+};
