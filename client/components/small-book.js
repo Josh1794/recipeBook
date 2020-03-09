b2ff71a60f2ea1696f6872a8d31d13f261cd4a4f
Joshua Grossman <Josh1794@gmail.com>
2020-03-09 15:50:44 -0400

    playing around with delete button

--- client/components/small-book.js
+++ client/components/small-book.js
@@ -5,28 +5,29 @@ export function SmallBook(props) {
   if (props.userId === props.user.id) {
     return (
       <div>
-        <a href={`/books/${props.id}`}>
-          <Card>
+        <Card>
+          <a href={`/books/${props.id}`}>
             <Card.Header className="bookHeader">{props.name}</Card.Header>
             <Card.Meta># of Recipes (coming soon)</Card.Meta>
             <Card.Description>{props.description}</Card.Description>
-            <div>
-              {props.isEditable ? (
-                <>
-                  <Button size="mini" circular icon="edit" />
-                  <Button
-                    color="red"
-                    size="mini"
-                    circular
-                    icon="trash alternate outline"
-                  />
-                </>
-              ) : (
-                <></>
-              )}
-            </div>
-          </Card>
-        </a>
+          </a>
+          <div>
+            {props.isEditable ? (
+              <>
+                <Button size="mini" circular icon="edit" />
+                <Button
+                  color="red"
+                  size="mini"
+                  circular
+                  icon="trash alternate outline"
+                  onClick={() => console.log(props.id)}
+                />
+              </>
+            ) : (
+              <></>
+            )}
+          </div>
+        </Card>
       </div>
     );
   } else return <></>;
